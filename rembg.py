e27d8fd4f00f8b927c4d968f2bf1061c881ecef9
zccrs <zccrs@live.com>
2025-05-19 11:48:37 +0800

    改成sciprt模式

--- rembg.py
+++ rembg.py
@@ -4,96 +4,82 @@ import os
 
 from modules import images
 from modules.processing import process_images, Processed
-from modules.processing import Processed
 from modules.shared import opts, cmd_opts, state
+from modules.paths_internal import models_path
+import rembg
 
+class Script(scripts.Script):
+    def title(self):
+        return "Remove Background"
 
-class Script(scripts.Script):  
+    def show(self, is_img2img):
+        return True
 
-# The title of the script. This is what will be displayed in the dropdown menu.
-    def title(self):
+    def ui(self, is_img2img):
+        models = [
+            "None",
+            "isnet-general-use",
+            "u2net",
+            "u2netp",
+            "u2net_human_seg",
+            "u2net_cloth_seg",
+            "silueta",
+            "isnet-anime",
+        ]
 
-        return "Flip/Rotate Output"
+        model = gr.Dropdown(label="Remove background model", choices=models, value="u2net")
+        return_mask = gr.Checkbox(label="Return mask", value=False)
+        alpha_matting = gr.Checkbox(label="Alpha matting", value=False)
 
+        with gr.Row(visible=False) as alpha_mask_row:
+            alpha_matting_erode_size = gr.Slider(label="Erode size", minimum=0, maximum=40, step=1, value=10)
+            alpha_matting_foreground_threshold = gr.Slider(label="Foreground threshold", minimum=0, maximum=255, step=1, value=240)
+            alpha_matting_background_threshold = gr.Slider(label="Background threshold", minimum=0, maximum=255, step=1, value=10)
 
-# Determines when the script should be shown in the dropdown menu via the 
-# returned value. As an example:
-# is_img2img is True if the current tab is img2img, and False if it is txt2img.
-# Thus, return is_img2img to only show the script on the img2img tab.
+        alpha_matting.change(
+            fn=lambda x: gr.update(visible=x),
+            inputs=[alpha_matting],
+            outputs=[alpha_mask_row],
+        )
 
-    def show(self, is_img2img):
+        overwrite = gr.Checkbox(label="Overwrite existing files", value=False)
+        return [model, return_mask, alpha_matting, alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size, overwrite]
 
-        return is_img2img
+    def run(self, p, model, return_mask, alpha_matting, alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size, overwrite):
+        if not model or model == "None":
+            return process_images(p)
 
-# How the script's is displayed in the UI. See https://gradio.app/docs/#components
-# for the different UI components you can use and how to create them.
-# Most UI components can return a value, such as a boolean for a checkbox.
-# The returned values are passed to the run method as parameters.
+        if "U2NET_HOME" not in os.environ:
+            os.environ["U2NET_HOME"] = os.path.join(models_path, "u2net")
 
-    def ui(self, is_img2img):
-        angle = gr.Slider(minimum=0.0, maximum=360.0, step=1, value=0,
-        label="Angle")
-        hflip = gr.Checkbox(False, label="Horizontal flip")
-        vflip = gr.Checkbox(False, label="Vertical flip")
-        overwrite = gr.Checkbox(False, label="Overwrite existing files")
-        return [angle, hflip, vflip, overwrite]
+        basename = ""
+        if not overwrite:
+            basename += f"rembg_{model}"
+            if return_mask:
+                basename += "_mask"
+            if alpha_matting:
+                basename += "_alpha"
+        else:
+            p.do_not_save_samples = True
 
-  
+        proc = process_images(p)
 
-# This is where the additional processing is implemented. The parameters include
-# self, the model object "p" (a StableDiffusionProcessing class, see
-# processing.py), and the parameters returned by the ui method.
-# Custom functions can be defined here, and additional libraries can be imported 
-# to be used in processing. The return value should be a Processed object, which is
-# what is returned by the process_images method.
+        for i in range(len(proc.images)):
+            proc.images[i] = rembg.remove(
+                proc.images[i],
+                session=rembg.new_session(model),
+                only_mask=return_mask,
+                alpha_matting=alpha_matting,
+                alpha_matting_foreground_threshold=alpha_matting_foreground_threshold,
+                alpha_matting_background_threshold=alpha_matting_background_threshold,
+                alpha_matting_erode_size=alpha_matting_erode_size,
+            )
 
-    def run(self, p, angle, hflip, vflip, overwrite):
+            if proc.info is None:
+                proc.info = {}
+            proc.info["Rembg"] = model
 
-        # function which takes an image from the Processed object, 
-        # and the angle and two booleans indicating horizontal and
-        # vertical flips from the UI, then returns the 
-        # image rotated and flipped accordingly
-        def rotate_and_flip(im, angle, hflip, vflip):
-            from PIL import Image
-            
-            raf = im
-            
-            if angle != 0:
-                raf = raf.rotate(angle, expand=True)
-            if hflip:
-                raf = raf.transpose(Image.FLIP_LEFT_RIGHT)
-            if vflip:
-                raf = raf.transpose(Image.FLIP_TOP_BOTTOM)
-            return raf
+            images.save_image(proc.images[i], p.outpath_samples, basename,
+                proc.seed + i, proc.prompt, opts.samples_format, info=proc.info, p=p)
 
-  
-
-        # If overwrite is false, append the rotation information to the filename
-        # using the "basename" parameter and save it in the same directory.
-        # If overwrite is true, stop the model from saving its outputs and
-        # save the rotated and flipped images instead.
-        basename = ""
-        if(not overwrite):
-            if angle != 0:
-                basename += "rotated_" + str(angle)
-            if hflip:
-                basename += "_hflip"
-            if vflip:
-                basename += "_vflip"
-        else:
-            p.do_not_save_samples = True
-
-
-        proc = process_images(p)
-
-        # rotate and flip each image in the processed images
-        # use the save_images method from images.py to save
-        # them.
-        for i in range(len(proc.images)):
-
-            proc.images[i] = rotate_and_flip(proc.images[i], angle, hflip, vflip)
-
-            images.save_image(proc.images[i], p.outpath_samples, basename,
-            proc.seed + i, proc.prompt, opts.samples_format, info= proc.info, p=p)
-
-        return proc
+        return proc
